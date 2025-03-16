61b9b11b6828ef69bff4a8cdef06b8fa5e8047a4
Don't open it <160379580+Manzoorahmed76@users.noreply.github.com>
2025-03-16 18:07:03 +0500

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,77 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+module.exports = {
+  metode: 'get',
+  endpoint: '/api/apk/playstore',
+  name: 'playstore',
+  category: 'APK',
+  example: 'query=free fire',
+  async run({ req, res }) {
+    const { query } = req.query
+
+    if (!query) {
+      return res.status(400).json({
+        status: false,
+        error: 'Query parameter is required',
+      })
+    }
+
+    try {
+      const result = await PlayStore(query)
+      res.status(200).json({
+        status: true,
+        data: result,
+      })
+    } catch (error) {
+      res.status(500).json({
+        status: false,
+        error: error.message || 'Internal Server Error',
+      })
+    }
+  },
+}
+
+function PlayStore(search) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data, status } = await axios.get(
+        `https://play.google.com/store/search?q=${search}&c=apps`
+      )
+      const hasil = []
+      const $ = cheerio.load(data)
+      $(
+        '.ULeU3b > .VfPpkd-WsjYwc.VfPpkd-WsjYwc-OWXEXe-INsAgc.KC1dQ.Usd1Ac.AaN0Dd.Y8RQXd > .VfPpkd-aGsRMb > .VfPpkd-EScbFb-JIbuQc.TAQqTe > a'
+      ).each((i, u) => {
+        const linkk = $(u).attr('href')
+        const nama = $(u).find('.j2FCNc > .cXFu1 > .ubGTjb > .DdYX5').text()
+        const developer = $(u)
+          .find('.j2FCNc > .cXFu1 > .ubGTjb > .wMUdtb')
+          .text()
+        const img = $(u).find('.j2FCNc > img').attr('src')
+        const rate = $(u)
+          .find('.j2FCNc > .cXFu1 > .ubGTjb > div')
+          .attr('aria-label')
+        const rate2 = $(u)
+          .find('.j2FCNc > .cXFu1 > .ubGTjb > div > span.w2kbF')
+          .text()
+        const link = `https://play.google.com${linkk}`
+
+        hasil.push({
+          link: link,
+          nama: nama ? nama : 'No name',
+          developer: developer ? developer : 'No Developer',
+          img: img ? img : 'https://i.ibb.co/G7CrCwN/404.png',
+          rate: rate ? rate : 'No Rate',
+          rate2: rate2 ? rate2 : 'No Rate',
+          link_dev: `https://play.google.com/store/apps/developer?id=${developer.split(' ').join('+')}`,
+        })
+      })
+      if (hasil.every((x) => x === undefined))
+        return resolve({ message: 'Tidak ada result!' })
+      resolve(hasil)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
